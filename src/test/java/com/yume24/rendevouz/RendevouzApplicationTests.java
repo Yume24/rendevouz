2a173f9ef2d4b0eeff365015d537f80d4addc0fd
Antoni Jaros <antonijaros04@gmail.com>
2026-08-19 19:11:42 +0200

    init commit

---
+++ src/test/java/com/yume24/rendevouz/RendevouzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yume24.rendevouz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendevouzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
